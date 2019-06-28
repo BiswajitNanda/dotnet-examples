26dce716f208f00192730ebf71687a85bd8e0814
Biswajit Nanda <telljeet@gmail.com>
2019-06-28 14:47:29 +0100

    Pushing the solution containing Properties files

---
+++ SamplePropertiesfile/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Configuration;
+
+namespace SamplePropertiesfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String companyName = ConfigurationManager.AppSettings["company"];
+            String userName = ConfigurationManager.AppSettings["username"];
+
+            //Console.WriteLine("hello");
+            Console.WriteLine(companyName);
+            //Console.WriteLine(userName);
+
+        }
+    }
+}
